472a14276cc8067f5af413cbe1e1be1de860be53
Gustavo Huarcaya <diavolo@gahd.net>
2025-07-06 23:07:20 -0500

    feat: add new pages

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,5 +1,4 @@
-from os.path import exists
-import json
+from http import HTTPStatus
 import re
 
 from flask import Blueprint, render_template, request, send_from_directory
@@ -7,8 +6,18 @@ from markdown import markdown
 import yaml
 
 from app.utils.constants import (
-    CODE_NOT_FOUND_MSG, CODES_DIR_PATH, CONTENTS_DIR_PATH, DIR_NOT_FOUND_MSG,
-    FILE_ERROR_READING_MSG, MD_HEADER_META_REGEX)
+    CODES_DIR_PATH,
+    CONTENTS_DIR_PATH,
+    FILE_ERROR_READING_MSG,
+    HTTP_STATUS_CODES_CATEGORIES,
+    MD_HEADER_META_REGEX,
+    NOT_FOUND_HTML_TEMPLATE,
+)
+from app.utils.files import (
+    get_page_404_content,
+    read_file_content,
+    read_http_status_files,
+)
 from app.utils.html import strip_tags
 from app.utils.md import MD_EXTENSIONS, MD_EXTENSION_CONFIGS
 
@@ -17,56 +26,28 @@ home = Blueprint("home", __name__)
 
 @home.route("/")
 def index():
-    if not exists(CODES_DIR_PATH):
-        return render_template("404.html", error_message=DIR_NOT_FOUND_MSG), 404
+    http_codes_categories = {
+        k: v.copy() for k, v in HTTP_STATUS_CODES_CATEGORIES.items()
+    }
 
     try:
-        with CODES_DIR_PATH.joinpath("classes.json").open() as classes_from_json:
-            http_codes_collection = json.load(classes_from_json)
-    except IOError:
-        return render_template("404.html", error_message=FILE_ERROR_READING_MSG.format("JSON")), 404
-
-    try:
-        with open(CONTENTS_DIR_PATH.joinpath("index.md"), "r", encoding="utf-8") as index_file:
+        with open(
+            CONTENTS_DIR_PATH.joinpath("index.md"), "r", encoding="utf-8"
+        ) as index_file:
             index_md_content = index_file.read()
-            headline = markdown(index_md_content.split("---")[2],
-                                extension_configs=MD_EXTENSION_CONFIGS)
+            headline = markdown(
+                index_md_content.split("---")[2], extension_configs=MD_EXTENSION_CONFIGS
+            )
     except IOError:
-        return render_template("404.html", error_message=FILE_ERROR_READING_MSG.format("MD"))
+        return render_template(
+            NOT_FOUND_HTML_TEMPLATE, error_message=FILE_ERROR_READING_MSG.format("MD")
+        )
 
-    for item in CODES_DIR_PATH.glob("*.md"):
-        if item.is_file():
-            try:
-                with open(item, "r", encoding="utf-8") as md_file:
-                    md_file_content = md_file.read()
-
-                md_content_divided = md_file_content.split("---")
-
-                file_content_meta = yaml.safe_load(md_content_divided[1])
-
-                code = {
-                    "code": file_content_meta["code"],
-                    "set": file_content_meta["set"],
-                    "title": file_content_meta["title"]
-                }
-
-                if str(code["set"]) in http_codes_collection.keys():
-                    if "codes" in http_codes_collection[str(code["set"])]:
-                        http_codes_collection[str(
-                            code["set"])]["codes"].append(code)
-                        http_codes_collection[str(code["set"])]["codes"] = sorted(
-                            http_codes_collection[str(
-                                code["set"])]["codes"], key=lambda item: item.get("code")
-                        )
-                    else:
-                        http_codes_collection[str(code["set"])]["codes"] = [
-                            code]
-            except:
-                continue
+    http_codes_categories = read_http_status_files()
 
     context = {
         "headline": headline,
-        "http_codes_collection": http_codes_collection
+        "http_codes_collection": http_codes_categories,
     }
 
     return render_template("index.html", **context)
@@ -74,12 +55,63 @@ def index():
 
 @home.route("/about/")
 def about():
-    return render_template("about.html")
+    try:
+        with open(
+            CONTENTS_DIR_PATH.joinpath("about.md"), "r", encoding="utf-8"
+        ) as about_file:
+            about_md_content = about_file.read()
+            headline = markdown(
+                about_md_content.split("---")[2], extension_configs=MD_EXTENSION_CONFIGS
+            )
+        context = {"headline": headline}
+
+        return render_template("about.html", **context)
+    except IOError:
+        return render_template(
+            NOT_FOUND_HTML_TEMPLATE, error_message=FILE_ERROR_READING_MSG.format("MD")
+        )
 
 
 @home.route("/contact/")
 def contact():
-    return render_template("contact.html")
+    try:
+        with open(
+            CONTENTS_DIR_PATH.joinpath("contact.md"), "r", encoding="utf-8"
+        ) as contact_file:
+            contact_md_content = contact_file.read()
+            headline = markdown(
+                contact_md_content.split("---")[2],
+                extension_configs=MD_EXTENSION_CONFIGS,
+            )
+        context = {"headline": headline}
+
+        return render_template("contact.html", **context)
+    except IOError:
+        return render_template(
+            NOT_FOUND_HTML_TEMPLATE, error_message=FILE_ERROR_READING_MSG.format("MD")
+        )
+
+
+@home.route("/license/")
+def license():
+    try:
+        with open("LICENSE", "r", encoding="utf-8") as license_file:
+            license_content = license_file.read()
+            # Remove lines starting with "Copyright (c)" to prevent spam
+            lines = license_content.split("\n")
+            filtered_lines = [
+                line for line in lines if not line.strip().startswith("Copyright (c)")
+            ]
+            license_content = "\n".join(filtered_lines)
+            headline = license_content
+        context = {"headline": headline}
+
+        return render_template("license.html", **context)
+    except IOError:
+        return render_template(
+            NOT_FOUND_HTML_TEMPLATE,
+            error_message=FILE_ERROR_READING_MSG.format("LICENSE"),
+        )
 
 
 @home.route("/humans.txt")
@@ -89,41 +121,40 @@ def humans():
 
 @home.route("/<int:code>/")
 def get_code(code: int):
-    if not exists(CODES_DIR_PATH):
-        return render_template("404.html", error_message=DIR_NOT_FOUND_MSG), 404
-
-    code_md_file_path = CODES_DIR_PATH.joinpath(f"{code}.md")
-
-    if not code_md_file_path.is_file():
-        return render_template("404.html", error_message=CODE_NOT_FOUND_MSG), 404
-
     try:
-        with open(code_md_file_path, "r", encoding="utf-8") as code_md_file:
-            code_md_content = code_md_file.read()
-    except IOError:
-        return render_template("404.html", error_message=FILE_ERROR_READING_MSG.format("MD")), 404
+        code_md_file_path = CODES_DIR_PATH.joinpath(f"{code}.md")
+        code_md_content = read_file_content(code_md_file_path)
+    except IOError as e:
+        error_content = get_page_404_content()
+        error_content = error_content if error_content != "" else str(e)
+        context = {
+            "error_message": error_content,
+        }
+        return render_template(NOT_FOUND_HTML_TEMPLATE, **context), HTTPStatus.NOT_FOUND
+
+    http_codes_categories = read_http_status_files()
 
     md_content_divided = code_md_content.split("---")
 
     content_meta = yaml.safe_load(md_content_divided[1])
-    content_body = markdown(md_content_divided[2],
-                            extensions=[*MD_EXTENSIONS],
-                            extension_configs=MD_EXTENSION_CONFIGS)
-    content_footnotes = markdown(md_content_divided[3],
-                                 extensions=[*MD_EXTENSIONS],
-                                 extension_configs=MD_EXTENSION_CONFIGS)
+    content_body = markdown(
+        md_content_divided[2],
+        extensions=[*MD_EXTENSIONS],
+        extension_configs=MD_EXTENSION_CONFIGS,
+    )
+    content_footnotes = markdown(
+        md_content_divided[3],
+        extensions=[*MD_EXTENSIONS],
+        extension_configs=MD_EXTENSION_CONFIGS,
+    )
 
-    content = markdown(re.sub(MD_HEADER_META_REGEX, '', code_md_content),
-                       extensions=[*MD_EXTENSIONS],
-                       extension_configs=MD_EXTENSION_CONFIGS)
+    content = markdown(
+        re.sub(MD_HEADER_META_REGEX, "", code_md_content),
+        extensions=[*MD_EXTENSIONS],
+        extension_configs=MD_EXTENSION_CONFIGS,
+    )
 
     page_description = strip_tags(content_body.split("\n")[0])
-
-    try:
-        with CODES_DIR_PATH.joinpath("classes.json").open() as classes_from_json:
-            http_codes_collection = json.load(classes_from_json)
-    except IOError:
-        return render_template("404.html", error_message=FILE_ERROR_READING_MSG.format("JSON")), 404
 
     context = {
         "page_description": page_description,
@@ -131,6 +162,6 @@ def get_code(code: int):
         "content_body": content_body,
         "content_footnotes": content_footnotes,
         "content": content,
-        "http_codes_collection": http_codes_collection
+        "http_codes_collection": http_codes_categories,
     }
     return render_template("code.html", **context)
