36cb1ebb61a65977c243d42d7dbd4dd96f39b831
Gustavo Huarcaya <diavolo@gahd.net>
2025-07-14 23:21:18 -0500

    chore: use sentry in production

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,28 @@
 from http import HTTPStatus
 import os
 
+from dotenv import load_dotenv
 from flask import Flask, render_template
+import sentry_sdk
 
 from app.routes.home import home
 from app.routes.api import api
 from app.utils.constants import NOT_FOUND_HTML_TEMPLATE, SERVER_ERROR_HTML_TEMPLATE
 
 
+load_dotenv()
+
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
+
+    is_production = os.getenv("FLASK_ENV") == "production"
+
+    if is_production:
+        sentry_sdk.init(
+            dsn=os.getenv("SENTRY_DSN"),
+            send_default_pii=True,
+        )
+
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         # a default secret that should be overridden by instance config
